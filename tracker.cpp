38a83b37663fa3bf3870d6893db106c20f0815ab
oyvsik <leet.bab@gmail.com>
2019-06-14 21:31:46 +0200

    initial push

---
+++ tracker.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Øyvind Sikkeland on 14.06.2019.
+//
+
+#include "tracker.h"
---
+++ tracker.h
@@ -0,0 +1,14 @@
+//
+// Created by Øyvind Sikkeland on 14.06.2019.
+//
+
+#ifndef SECUREPIGEON_TRACKER_H
+#define SECUREPIGEON_TRACKER_H
+
+
+class tracker {
+
+};
+
+
+#endif //SECUREPIGEON_TRACKER_H
